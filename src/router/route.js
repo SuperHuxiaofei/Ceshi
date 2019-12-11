56b3bf1179a2f89ee9e4e5ebd834fddba9268271
Hyler7 <54178833+Hyler7@users.noreply.github.com>
2019-12-11 10:58:04 +0800

    b

--- src/router/route.js
+++ src/router/route.js
@@ -1,4 +1,4 @@
-import {
+/* import {
   Login,
   NotFound,
   Homepage,
@@ -6,51 +6,73 @@ import {
   ArticleEdit,
   Setting,
   PostContent
-} from "../pages"
+} from "../pages" */
+import Loadable from "react-loadable"
+import Loading from "../../src/components/Loading"
 
 export const mainRouter = [
   {
-    pathname:"/login",
-    component:Login
+    pathname: "/login",
+    component: Loadable({
+      loader: () => import("../../src/pages/login"),
+      loading: () => Loading
+    })
   },
   {
-    pathname:"/404",
-    component:NotFound
+    pathname: "/404",
+    component: Loadable({
+      loader: () => import("../../src/pages/notFound"),
+      loading: () => Loading
+    })
   }
 ]
 export const adminRouter = [
   {
-    pathname:"/admin/homepage",
-    component:Homepage,
-    title:"个人主页",
-    isOff:true,
-    icon:"dashboard"
+    pathname: "/admin/homepage",
+    component: Loadable({
+      loader: () => import("../../src/pages/homepage"),
+      loading: () => Loading
+    }),
+    title: "个人主页",
+    isOff: true,
+    icon: "dashboard"
   },
   {
-    pathname:"/admin/articleList",
-    component:ArticleList,
-    title:"热门话题",
-    isOff:true,
-    icon:"unordered-list"
+    pathname: "/admin/articleList",
+    component: Loadable({
+      loader: () => import("../../src/pages/article/ArticleList"),
+      loading: () => Loading
+    }),
+    title: "热门话题",
+    isOff: true,
+    icon: "unordered-list"
   },
   {
-    pathname:"/admin/articleEdit",
-    component:ArticleEdit,
-    isOff:false
+    pathname: "/admin/articleEdit",
+    component: Loadable({
+      loader: () => import("../../src/pages/article/ArticleEdit"),
+      loading: () => Loading
+    }),
+    isOff: false
   },
   {
-    pathname:"/admin/postContent",
-    component:PostContent,
-    title:"发布内容",
-    isOff:true,
-    icon:"plus-circle"
+    pathname: "/admin/postContent",
+    component: Loadable({
+      loader: () => import("../../src/pages/postContent"),
+      loading: () => Loading
+    }),
+    title: "发布内容",
+    isOff: true,
+    icon: "plus-circle"
   },
   {
-    pathname:"/admin/setting",
-    component:Setting,
-    title:"设置",
-    isOff:true,
-    icon:"setting"
+    pathname: "/admin/setting",
+    component: Loadable({
+      loader: () => import("../../src/pages/setting"),
+      loading: () => Loading
+    }),
+    title: "设置",
+    isOff: true,
+    icon: "setting"
   }
-
 ]
