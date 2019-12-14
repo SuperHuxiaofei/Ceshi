5f0edb5d9496e151f97f22d8fe60d3e51cc99c6b
Hyler7 <54178833+Hyler7@users.noreply.github.com>
2019-12-14 14:54:15 +0800

    Update Header.js

--- src/pages/login/components/Header.js
+++ src/pages/login/components/Header.js
@@ -1,11 +1,11 @@
 import React,{Component,Fragment} from "react"
-import logo from "../../../assets/img/QQ图片20191214144406.png"
+import logo from "@/assets/img/QQ图片20191214144406.png"
 class LoginHeader extends Component {
   render(){
     return (
       <Fragment>
         <img src={logo} className="login-logo"/>
-        <h1>Welcome to NB CRM</h1>
+        <h1>Welcome to NB FSC</h1>
       </Fragment>
     )
   }
